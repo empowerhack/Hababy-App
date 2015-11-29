db6a6d45266809d793b6905e3ed99f27c2e0c829
Rosie-Brigham <rosiebrigham@gmail.com>
2015-11-29 16:16:23 +0000

    adds arabic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,13 @@ class WelcomeController < ApplicationController
 
   def index
   end
+
+  def english
+    session[:language] = "ENG"
+  end
+
+  def arabic
+    session[:language] = "ARB"
+  end
 end
 
